5ca4d467552fa044f4844d13632aae9db5a26e6b
Elisa Demanez <demanez.elisa@gmail.com>
2017-12-08 09:40:25 +0100

    Le compte & fonction alert tout les 20 clics fonctionne. console.log en commentaires.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-	//votre code
+	var compte = 0;
+	$('img').click(
+		function(){
+			// console.log('ok');
+			compte ++;
+			// console.log(compte);
+			$('span').html(compte);
 
+			if ((compte%20) == 0){
+				alert( 'You nerd' );
+			}
+
+	
+		});
 
 });
